c677011082348bc5e066dd9abc9b271c39773df7
jaycee-github <jayceewakanda@gmail.com>
2023-07-12 17:25:15 +0800

    Added swaggers docs

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // connect DB
 const connectDB = require("./db/connect.js");
 
@@ -37,8 +42,11 @@ app.use(cors());
 app.use(xss());
 
 app.get("/", (req, res) => {
-    return res.send("Jobs API");
+    // return res.send("Jobs API");
+    return res.send(`<h1>Jobs API</h1><a href="/api-docs">Documentation</a>`);
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
